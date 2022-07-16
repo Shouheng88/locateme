5a7b1af55a5f88192740bca9a512d5516c717f3f
Jeff Wang <shouheng2015@gmail.com>
2022-07-16 11:54:11 +0800

    feat: add java directory scanner

---
+++ child-module/src/main/java/me/shouheng/module/Utils.kt
@@ -0,0 +1,3 @@
+package me.shouheng.module
+
+class Utils
--- plugin/src/main/java/me/shouheng/locate/LocatePlugin.kt
+++ plugin/src/main/java/me/shouheng/locate/LocatePlugin.kt
@@ -1,11 +1,31 @@
 package me.shouheng.locate
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.SourceSet
 
 /** The plugin. */
-class LocatePlugin : Plugin<Project> {
-    override fun apply(p0: Project) {
-        println("Locate plugin comes into effect.")
+class LocatePlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+        project.gradle.projectsEvaluated {
+            project.rootProject.allprojects.forEach { p ->
+                p.extensions.findByType(LibraryExtension::class.java)?.let {
+                    it.sourceSets.findByName(SourceSet.MAIN_SOURCE_SET_NAME)?.let { set ->
+                        set.java.srcDirs.forEach { file ->
+                            println("**** java dir: [$file]")
+                        }
+                    }
+                }
+                p.extensions.findByType(AppExtension::class.java)?.let {
+                    it.sourceSets.findByName(SourceSet.MAIN_SOURCE_SET_NAME)?.let { set ->
+                        set.java.srcDirs.forEach { file ->
+                            println("**** java dir: [$file]")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
