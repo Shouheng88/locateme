a3ee718bf1f5e05dea2b96209151a2fe43eea065
Jeff Wang <shouheng2015@gmail.com>
2022-07-16 17:28:58 +0800

    feat: refine constant pool parser code

--- plugin/src/main/java/me/shouheng/locate/ConstantPool.kt
+++ plugin/src/main/java/me/shouheng/locate/ConstantPool.kt
@@ -6,12 +6,19 @@ import java.nio.charset.Charset
 /** Parse constant pool. */
 object ConstantPool {
 
-    private val constants = mapOf(
+    private val CONSTANTS_LENGTHS = mapOf(
         3 to 4,  4 to 4,  5 to 8,  6 to 8,
         7 to 2,  8 to 2,  9 to 4,  10 to 4,
         11 to 4, 12 to 4, 15 to 3, 16 to 2,
         17 to 4, 18 to 4, 19 to 2, 20 to 2
     )
+
+    private const val CONSTANT_UTF8     = 1
+    private const val CONSTANT_INT      = 3
+    private const val CONSTANT_FLOAT    = 4
+    private const val CONSTANT_LONG     = 5
+    private const val CONSTANT_DOUBLE   = 6
+    private const val CONSTANT_CLASS    = 7
 
     /** Parse constant pool of class file path. */
     fun parse(path: String): List<String> {
@@ -51,7 +58,7 @@ object ConstantPool {
         val classNos = mutableListOf<Int>()
         while (current < count) {
             when (val code = bytes[index].toInt()) {
-                1 -> {
+                CONSTANT_UTF8 -> {
                     val length = bytes[index+1]*16*16 + bytes[index+2]
                     val start = index + 3
                     val end = start + length
@@ -61,18 +68,19 @@ object ConstantPool {
                     index += (2 + length + 1)
                     current += 1
                 }
-                5, 6 -> {
-                    // Special guideline for double and long type, see
+                CONSTANT_LONG, CONSTANT_DOUBLE -> {
+                    // Special guideline for double and long type, see,
                     // https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.4.5
-                    index += (constants[code]!! + 1)
+                    index += (CONSTANTS_LENGTHS[code]!! + 1)
                     current += 2
                 }
                 else -> {
-                    if (code == 7) {
+                    if (code == CONSTANT_CLASS) {
+                        // Specify witch constant in pool is class.
                         val no = bytes[index+1]*16*16 + bytes[index+2]
                         classNos.add(no)
                     }
-                    index += (constants[code]!! + 1)
+                    index += (CONSTANTS_LENGTHS[code]!! + 1)
                     current += 1
                 }
             }
